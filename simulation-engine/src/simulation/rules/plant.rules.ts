24869f6ca1e5f19eeb27e312f06ddbfccb1a30ba
shammas-kp <149519186+shammas-kp@users.noreply.github.com>
2026-05-09 13:15:14 +0530

    all rule case added

--- simulation-engine/src/simulation/rules/plant.rules.ts
+++ simulation-engine/src/simulation/rules/plant.rules.ts
@@ -3,6 +3,35 @@ import { Level } from '../types/level.enum';
 
 export const plantRules: Rule[] = [
 
+  // LOW WATER + LOW SUNLIGHT
+  {
+    condition: {
+      water: Level.LOW,
+      sunlight: Level.LOW,
+    },
+
+    effects: {
+      growth: -10,
+      health: -10,
+      stress: +10,
+    },
+  },
+
+  // LOW WATER + MEDIUM SUNLIGHT
+  {
+    condition: {
+      water: Level.LOW,
+      sunlight: Level.MEDIUM,
+    },
+
+    effects: {
+      growth: -8,
+      health: -12,
+      stress: +15,
+    },
+  },
+
+  // LOW WATER + HIGH SUNLIGHT
   {
     condition: {
       water: Level.LOW,
@@ -10,12 +39,27 @@ export const plantRules: Rule[] = [
     },
 
     effects: {
-      health: -20,
-      stress: +15,
       growth: -5,
+      health: -20,
+      stress: +20,
     },
   },
 
+  // MEDIUM WATER + LOW SUNLIGHT
+  {
+    condition: {
+      water: Level.MEDIUM,
+      sunlight: Level.LOW,
+    },
+
+    effects: {
+      growth: +2,
+      health: -5,
+      stress: +5,
+    },
+  },
+
+  // MEDIUM WATER + MEDIUM SUNLIGHT
   {
     condition: {
       water: Level.MEDIUM,
@@ -23,12 +67,27 @@ export const plantRules: Rule[] = [
     },
 
     effects: {
-      health: +5,
       growth: +12,
+      health: +5,
       stress: -5,
     },
   },
 
+  // MEDIUM WATER + HIGH SUNLIGHT
+  {
+    condition: {
+      water: Level.MEDIUM,
+      sunlight: Level.HIGH,
+    },
+
+    effects: {
+      growth: +15,
+      health: +3,
+      stress: +5,
+    },
+  },
+
+  // HIGH WATER + LOW SUNLIGHT
   {
     condition: {
       water: Level.HIGH,
@@ -36,9 +95,38 @@ export const plantRules: Rule[] = [
     },
 
     effects: {
-      diseaseRisk: +20,
-      health: -10,
       growth: -2,
+      health: -10,
+      diseaseRisk: +20,
+    },
+  },
+
+  // HIGH WATER + MEDIUM SUNLIGHT
+  {
+    condition: {
+      water: Level.HIGH,
+      sunlight: Level.MEDIUM,
+    },
+
+    effects: {
+      growth: +5,
+      health: -2,
+      diseaseRisk: +10,
+      stress: +3,
+    },
+  },
+
+  // HIGH WATER + HIGH SUNLIGHT
+  {
+    condition: {
+      water: Level.HIGH,
+      sunlight: Level.HIGH,
+    },
+
+    effects: {
+      growth: +10,
+      health: +2,
+      stress: +8,
     },
   },
 ];
